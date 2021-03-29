f2bbccbd26de2003583d1689a46e3bed4a7228b8
sherry242 <71978502+sherry242@users.noreply.github.com>
2021-03-29 17:03:05 +0100

    Update search.py

--- app/search.py
+++ app/search.py
@@ -1,7 +1,7 @@
 import os
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template,current_app, request,Blueprint, flash
-from .flask_paginate1 import Pagination, get_page_parameter,get_per_page_parameter
+from flask import Flask, render_template, current_app, request, Blueprint, flash
+from .flask_paginate1 import Pagination, get_page_parameter, get_per_page_parameter
 import pandas
 
 # 数组传参
@@ -9,16 +9,18 @@ import pandas
 
 # 字典传参
 num = {
-	1: ['10001', '123', 'Mike', '2017-5-6'],
-	2: ['10002', '123', 'Sam', '2018-5-6'],
-	3: ['10003', '123', 'hong', '2019-5-6'],
-	4: ['10004', '123', 'xiao', '2019-5-6']
+    1: ['10001', '123', 'Mike', '2017-5-6', 'lalalalal'],
+    2: ['10002', '123', 'Sam', '2018-5-6', 'lalalalal'],
+    3: ['10003', '123', 'hong', '2019-5-6', 'lalalalal'],
+    4: ['10004', '123', 'xiao', '2019-5-6', 'lalalalal']
 }
-data_pd = pandas.DataFrame.from_dict(num,orient='index',columns=['Songs','Albums','Artists','date'])
-data_dict = data_pd.to_dict(orient = 'records')
+data_pd = pandas.DataFrame.from_dict(num, orient='index', columns=['Songs', 'Albums', 'Artists', 'date', 'Lyrics'])
+data_dict = data_pd.to_dict(orient='records')
+
 
 def get_users(offset=0, per_page=20):
     return data_dict[offset: offset + per_page]
+
 
 bp = Blueprint('search', __name__, url_prefix='/')
 
@@ -27,111 +29,111 @@ bp = Blueprint('search', __name__, url_prefix='/')
 def index():
     return render_template('index.html')
 
+
 @bp.route('/search_Songs', methods=('GET', 'POST'))
 def search_Songs():
-	page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-	total = len(data_dict)
-	pagination_users = get_users(offset=offset, per_page=per_page)
-	pagination = Pagination(page=page, per_page=per_page, total=total,
-							css_framework='bootstrap4')
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    total = len(data_dict)
 
-	return render_template('music.html',
-						   data=pagination_users,
-						   page=page,
-						   per_page=per_page,
-						   pagination=pagination,
-						   data_num=data_num,
-						   time=time
-						   )
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
 
 
-@bp.route('/search_Artists',methods=('GET', 'POST'))
+    return render_template('music.html',
+                           data=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           kwd=keyword,
+                           data_num=data_num,
+                           time=time
+                           )
+
+
+@bp.route('/search_Artists', methods=('GET', 'POST'))
 def search_Artists():
-	page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-	total = len(data_dict)
-	pagination_users = get_users(offset=offset, per_page=per_page)
-	pagination = Pagination(page=page, per_page=per_page, total=total,
-							css_framework='bootstrap4')
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    total = len(data_dict)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
 
-	return render_template('artists.html',
-						   data=pagination_users,
-						   page=page,
-						   per_page=per_page,
-						   pagination=pagination,
-						   data_num=data_num,
-						   time=time
-						   )
+    return render_template('artists.html',
+                           data=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           kwd=keyword,
+                           data_num=data_num,
+                           time=time
+                           )
 
-@bp.route('/search_Albums',methods=('GET', 'POST'))
+
+@bp.route('/search_Albums', methods=('GET', 'POST'))
 def search_Albums():
-	page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-	total = len(data_dict)
-	pagination_users = get_users(offset=offset, per_page=per_page)
-	pagination = Pagination(page=page, per_page=per_page, total=total,
-							css_framework='bootstrap4')
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    total = len(data_dict)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
 
-	return render_template('albums.html',
-						   data=pagination_users,
-						   page=page,
-						   per_page=per_page,
-						   pagination=pagination,
-						   data_num=data_num,
-						   time=time
-						   )
+    return render_template('albums.html',
+                           data=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           kwd=keyword,
+                           data_num=data_num,
+                           time=time
+                           )
 
-@bp.route('/search_Lyrics',methods=('GET', 'POST'))
+
+@bp.route('/search_Lyrics', methods=('GET', 'POST'))
 def search_Lyrics():
-	page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-	total = len(data_dict)
-	pagination_users = get_users(offset=offset, per_page=per_page)
-	pagination = Pagination(page=page, per_page=per_page, total=total,
-							css_framework='bootstrap4')
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    total = len(data_dict)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
 
-	return render_template('lyrics.html',
-						   data=pagination_users,
-						   page=page,
-						   per_page=per_page,
-						   pagination=pagination,
-						   data_num=data_num,
-						   time=time
-						   )
+    return render_template('lyrics.html',
+                           data=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           kwd=keyword,
+                           data_num=data_num,
+                           time=time
+                           )
 
 
 @bp.route('/search', methods=('GET', 'POST'))
 def search():
-	Result = ''
-	choice = ''
-	# if request.method == 'POST':
-	# 	# 	content = request.form['search']
-	# 	# 	choice = request.values.get("search_method")
-	# 	# 	Result = "search method: "+choice+"\nsearch content: "+content
-	# 	# 	# print(Result)
-	# 	# 	error = None
-	# 	#
-	# 	# 	if not content:
-	# 	# 		error = 'Content Cannot be None.'
-	# 	#
-	# 	# 	flash(error)
 
-	page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-	total = len(data_dict)
-	pagination_users = get_users(offset=offset, per_page=per_page)
-	pagination = Pagination(page=page, per_page=per_page, total=total,
-							css_framework='bootstrap4')
+    global keyword
+    keyword = request.form['keyword']
 
-	return render_template('music.html',
-						   data=pagination_users,
-						   page=page,
-						   per_page=per_page,
-						   pagination=pagination,
-						   kwd=keyword,
-						   data_num=data_num,
-						   time=time)
+    page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+    total = len(data_dict)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
 
+
+
+    return render_template('music.html',
+                           data=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           kwd=keyword,
+                           data_num=data_num,
+                           time=time)
 
 
 def get_page_args(
-    page_parameter=None, per_page_parameter=None, for_test=False, **kwargs
+        page_parameter=None, per_page_parameter=None, for_test=False, **kwargs
 ):
     """param order: 1. passed parameter 2. request.args 3: config value
     for_test will return page_parameter and per_page_parameter"""
@@ -150,7 +152,7 @@ def get_page_args(
     page = int(args.get(page_name, 1, type=int))
     per_page = args.get(per_page_name, type=int)
     if not per_page:
-        per_page = int(current_app.config.get(per_page_name.upper(), 3))   #这里改per_page
+        per_page = int(current_app.config.get(per_page_name.upper(), 3))  # 这里改per_page
     else:
         per_page = int(per_page)
 
@@ -159,10 +161,11 @@ def get_page_args(
 
 
 def algorithm(content):
-	return content
+    return content
+
 
 ##############################################################################################################
-keyword='Mike'
-data_num=len(data_dict)
-time='0.3451s'
+
+data_num = len(data_dict)
+time = '0.3451s'
 #############################################################################################################
